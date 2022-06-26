603e9c21f88558225a9c186ee35aebcc9b827d88
umeshyogi29 <108227553+umeshyogi29@users.noreply.github.com>
2022-06-26 22:51:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+(function () {
+  "use strict";
+
+  var todoApp = (function () {
+    document.addEventListener("DOMContentLoaded", function () {
+      init();
+    });
+
+    var init = function () {
+      document.getElementById("add-item").addEventListener(
+        "click",
+        function () {
+          var newTitle = document.getElementById("title").value,
+            newContent = document.getElementById("content").value;
+          if (validate(newTitle)) {
+            addTodo(newTitle, newContent);
+          }
+        },
+        false
+      );
+
+      document.getElementById("item").addEventListener(
+        "click",
+        function (e) {
+          if (e.target && e.target.nodeName == "BUTTON") {
+            deleteTodo(e.target.parentNode);
+          }
+        },
+        false
+      );
+    };
+
+    var validate = function (newTitle, newContent) {
+      if (newTitle.length === 0) {
+        var target = document.getElementById("title");
+        target.className = target.className + " error";
+        return false;
+      } else {
+        return true;
+      }
+    };
+
+    var addTodo = function (title, content) {
+      var newTodo = document.createElement("section"),
+        h2 = document.createElement("h2"),
+        newTitle = document.createTextNode(title),
+        p = document.createElement("p"),
+        newContent = document.createTextNode(content),
+        button = document.createElement("button"),
+        deleteBtn = document.createTextNode("delete");
+
+      h2.appendChild(newTitle);
+      newTodo.appendChild(h2);
+      p.appendChild(newContent);
+      newTodo.appendChild(p);
+      button.appendChild(deleteBtn);
+      newTodo.appendChild(button);
+
+      document.getElementById("item").appendChild(newTodo);
+      document.getElementById("form").reset();
+    };
+
+    var deleteTodo = function (todo) {
+      todo.parentNode.removeChild(todo);
+    };
+  })();
+})();
